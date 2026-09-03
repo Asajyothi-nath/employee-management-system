999ac33849976f6934797b27b43c9fbca12180b6
Asajyothi-nath <ashanath2023@gmail.com>
2026-09-03 21:47:21 +0530

    Changed Some urls

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,23 @@ const app = express();
 // MIDDLEWARES
 app.use(express.json());
 
-app.use(cors({
+/*app.use(cors({
 
     origin: "http://localhost:5173",
 
     credentials: true
+}));*/
+
+app.use(cors({
+    origin: [
+        "http://localhost:5173", 
+        "https://vercel.app"
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 
 app.use(cookieParser());
 
