bed34a208849207133935d2632f84b299202f555
Asajyothi-nath <ashanath2023@gmail.com>
2026-09-03 21:08:12 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // This tells Vite to swap the local text with your Render URL during deployment builds
+    'http://localhost:8000/api': JSON.stringify('https://onrender.com'),
+  }
 })
